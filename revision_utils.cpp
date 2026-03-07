f84940bb912c1c1eeed4865b4639ccd0e67c01c9
IU8LMC <iu8lmc@users.noreply.github.com>
2026-03-07 13:50:19 +0100

    Build 2603070005: Dual Carrier FT2, freq-diversity DXped, fstep 200→500Hz, fix update checker

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -80,6 +80,6 @@ QString version (bool include_patch)
 
 QString program_title (QString const& revision)
 {
-  QString id {"Decodium v3.0 FT2 Raptor v3.0.2603070004"};
+  QString id {"Decodium v3.0 FT2 Raptor v3.0.2603070005"};
   return id + " " + revision;
 }
